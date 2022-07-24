5235aadfb5b213781522297d7afbd57cce063c26
Jaakko Pyrhönen <103743015+jake52227@users.noreply.github.com>
2022-07-24 12:23:03 +0300

    lisätty virheentarkistus

--- application/MovieTrackerV2/app/src/main/java/com/example/movietrackerv2/data/xml_tool/Parser.java
+++ application/MovieTrackerV2/app/src/main/java/com/example/movietrackerv2/data/xml_tool/Parser.java
@@ -6,7 +6,11 @@ import org.w3c.dom.NodeList;
 public class Parser implements ParserInterface {
     // parses the given document to a NodeList by the given tag. Returns the list.
     public NodeList parseToNodeList(Document doc, String tag) {
-        doc.getDocumentElement().normalize();
-        return doc.getDocumentElement().getElementsByTagName(tag);
+        try {
+            doc.getDocumentElement().normalize();
+            return doc.getDocumentElement().getElementsByTagName(tag);
+        } catch (NullPointerException e) {
+            return null;
+        }
     }
 }
